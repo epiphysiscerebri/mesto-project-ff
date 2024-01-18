a2b991dea8f42e57b1138871042953bdf4ab6d67
Anton Zamuriev <zamurievanton@inbox.ru>
2024-01-18 18:14:06 +0300

    Добавлено отображение и удаление на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardList = document.querySelector('.places__list')
+const cardTemplate = document.querySelector('#card-template').content
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
-
+function cardRemove (e) {
+  e.target.parentElement.remove()
+}
 // @todo: Вывести карточки на страницу
+function loadingCards(card, cardRemove) {
+  const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true)
+  cardTemplateClone.querySelector('.card__delete-button').addEventListener('click', cardRemove)
+
+  cardTemplateClone.querySelector('.card__image').src = card.link
+  cardTemplateClone.querySelector('.card__description').querySelector('.card__title').textContent = card.name
+  
+  cardList.append(cardTemplateClone)
+
+}
+
+initialCards.forEach(el => {
+  loadingCards(el, cardRemove)
+})
+
+
+
