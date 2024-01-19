00f37f4ec88bfbf0338ec7aef1d2671d56fc6fd3
Anton Zamuriev <zamurievanton@inbox.ru>
2024-01-19 15:34:15 +0300

    доработка функции удаления

--- scripts/index.js
+++ scripts/index.js
@@ -6,23 +6,25 @@ const cardTemplate = document.querySelector('#card-template').content
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
-function cardRemove (e) {
-  e.target.parentElement.remove()
+function cardRemove (card) {
+  card.remove()
 }
 // @todo: Вывести карточки на страницу
-function loadingCards(card, cardRemove) {
+function createCard(card, cardRemove) {
   const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true)
-  cardTemplateClone.querySelector('.card__delete-button').addEventListener('click', cardRemove)
+  cardTemplateClone.querySelector('.card__delete-button').addEventListener('click', () => cardRemove(cardTemplateClone))
 
   cardTemplateClone.querySelector('.card__image').src = card.link
   cardTemplateClone.querySelector('.card__description').querySelector('.card__title').textContent = card.name
+  cardTemplateClone.querySelector('.card__image').alt = card.name
   
-  cardList.append(cardTemplateClone)
+  return cardTemplateClone
 
 }
 
+
 initialCards.forEach(el => {
-  loadingCards(el, cardRemove)
+  cardList.append(createCard(el, cardRemove))
 })
 
 
