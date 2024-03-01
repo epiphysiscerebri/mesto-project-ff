459b484c49ac12c695648472822827928f502344
Anton Zamuriev <zamurievanton@inbox.ru>
2024-03-02 00:44:52 +0300

    Добавил разделение на модули

---
+++ src/scripts/card.js
@@ -0,0 +1,28 @@
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content
+
+// @todo: Функция удаления карточки
+function removeCard(card) {
+  card.remove()
+}
+
+// @todo: Функция лайка карточки
+function likeCard(card) {
+  card.querySelector('.card__like-button').classList.toggle('card__like-button_is-active')
+}
+
+// @todo: Функция создания карточки
+function createCard(card, removeCard, likeCard) {
+  const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true)
+  cardTemplateClone.querySelector('.card__delete-button').addEventListener('click', () => removeCard(cardTemplateClone))
+  cardTemplateClone.querySelector('.card__like-button').addEventListener('click', () => likeCard(cardTemplateClone))
+
+  cardTemplateClone.querySelector('.card__image').src = card.link
+  cardTemplateClone.querySelector('.card__description').querySelector('.card__title').textContent = card.name
+  cardTemplateClone.querySelector('.card__image').alt = card.name
+  
+  return cardTemplateClone
+
+}
+
+export {removeCard, likeCard, createCard}
