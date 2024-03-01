197128479dd0249cb423bc8c939a6b9a6afd7338
Anton Zamuriev <zamurievanton@inbox.ru>
2024-03-01 23:47:30 +0300

    Добавлена функция лайков

--- src/index.js
+++ src/index.js
@@ -11,13 +11,20 @@ const profileDescription = page.querySelector('.profile__description')
 const forms = document.forms
 
 // @todo: Функция удаления карточки
-function cardRemove (card) {
+function cardRemove(card) {
   card.remove()
 }
+
+// @todo: Функция лайка карточки
+function likeCard(card) {
+  card.querySelector('.card__like-button').classList.toggle('card__like-button_is-active')
+}
+
 // @todo: Функция создания карточки
-function createCard(card, cardRemove) {
+function createCard(card, cardRemove, likeCard) {
   const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true)
   cardTemplateClone.querySelector('.card__delete-button').addEventListener('click', () => cardRemove(cardTemplateClone))
+  cardTemplateClone.querySelector('.card__like-button').addEventListener('click', () => likeCard(cardTemplateClone))
 
   cardTemplateClone.querySelector('.card__image').src = card.link
   cardTemplateClone.querySelector('.card__description').querySelector('.card__title').textContent = card.name
@@ -25,24 +32,6 @@ function createCard(card, cardRemove) {
   
   return cardTemplateClone
 
-}
-
-// @todo: Функция сохранения записей при создании и при редактировании
-function savedData(evt, flag) {
-  if(flag == 'edit') {
-    profileTitle.textContent = document.querySelector('.popup__input_type_name').value
-    profileDescription.textContent = document.querySelector('.popup__input_type_description').value
-    page.querySelector('.popup_type_edit').style.display = 'none'
-  } else if(flag == 'add'){
-    if (forms.new_place.querySelector('.popup__input_type_card-name').value.length && forms.new_place.querySelector('.popup__input_type_url').value.length) {
-      let card = {
-        name: forms.new_place.querySelector('.popup__input_type_card-name').value,
-        link: forms.new_place.querySelector('.popup__input_type_url').value
-      }
-      page.querySelector('.popup_type_new-card').style.display = 'none'
-      cardList.prepend(createCard(card, cardRemove))
-    }
-  }
 }
 
 // @todo: Функция закрытия попапа
@@ -62,16 +51,16 @@ function closePopup(evt) {
     page.querySelector('.popup_type_image').style.display = 'none'
   } else if(evt.target.classList.contains('popup__button')) {
     if (page.querySelector('.popup_type_edit').style.display === 'flex') {
-      savedData(evt, 'edit')
+      savedData('edit')
     } else if (page.querySelector('.popup_type_new-card').style.display === 'flex') {
-      savedData(evt, 'add')
+      savedData('add')
     }
   }
 }
 
 // @todo: Функция открытия попапа
 function openPopup(element) {
-  listenersAdder() 
+  adderListeners() 
 
   // Добавление обработчика при отправке на формы  
   for (let form of forms) {
@@ -96,8 +85,26 @@ function openPopup(element) {
   }
 }
 
+// @todo: Функция сохранения записей при создании и при редактировании
+function savedData(flag) {
+  if(flag == 'edit') {
+    profileTitle.textContent = document.querySelector('.popup__input_type_name').value
+    profileDescription.textContent = document.querySelector('.popup__input_type_description').value
+    page.querySelector('.popup_type_edit').style.display = 'none'
+  } else if(flag == 'add'){
+    if (forms.new_place.querySelector('.popup__input_type_card-name').value.length && forms.new_place.querySelector('.popup__input_type_url').value.length) {
+      let card = {
+        name: forms.new_place.querySelector('.popup__input_type_card-name').value,
+        link: forms.new_place.querySelector('.popup__input_type_url').value
+      }
+      page.querySelector('.popup_type_new-card').style.display = 'none'
+      cardList.prepend(createCard(card, cardRemove, likeCard))
+    }
+  }
+}
+
 // todo: Функция навешивания слушателей событий
-function listenersAdder() {
+function adderListeners() {
   page.querySelectorAll('.popup__close').forEach((el) => {
     el.addEventListener('click', closePopup)
   })
@@ -112,6 +119,6 @@ page.addEventListener('click', (evt) => {
 })
 
 initialCards.forEach(el => {
-  cardList.append(createCard(el, cardRemove))
+  cardList.append(createCard(el, cardRemove, likeCard))
 })
 
